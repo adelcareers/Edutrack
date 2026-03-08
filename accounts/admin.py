56c1f99059a4061bd8f1a669302d995711e2508c
adelcareers <adel.careers@gmail.com>
2026-03-08 22:31:40 +0000

    feat(accounts): configure UserAdmin with role and subscription fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileInline(admin.StackedInline):
+    """Inline editor for UserProfile shown inside the User admin detail view."""
+
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Extended User admin that embeds the UserProfile inline."""
+
+    inlines = (UserProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    """Admin view for UserProfile with role and subscription filters."""
+
+    list_display = ['user', 'role', 'subscription_active', 'created_at']
+    list_filter = ['role', 'subscription_active']
+    search_fields = ['user__username', 'user__email']
