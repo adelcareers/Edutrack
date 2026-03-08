f122bebedd47daa933721a6e5b165f43cc086976
adelcareers <adel.careers@gmail.com>
2026-03-08 22:31:47 +0000

    feat(tracker): register tracker models in Django admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import LessonLog, EvidenceFile
 
-# Register your models here.
+
+@admin.register(LessonLog)
+class LessonLogAdmin(admin.ModelAdmin):
+    """Admin view for lesson completion records with status and mastery filters."""
+
+    list_display = ['scheduled_lesson', 'status', 'mastery', 'completed_at']
+    list_filter = ['status', 'mastery']
+    search_fields = ['scheduled_lesson__lesson__lesson_title']
+
+
+@admin.register(EvidenceFile)
+class EvidenceFileAdmin(admin.ModelAdmin):
+    """Admin view for student-uploaded evidence files."""
+
+    list_display = ['original_filename', 'lesson_log', 'uploaded_by', 'uploaded_at']
+    list_filter = ['uploaded_at']
+    search_fields = ['original_filename']
