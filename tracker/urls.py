e166d81b74122951abdf28f663dfb2c9e812f92f
adelcareers <adel.careers@gmail.com>
2026-03-09 13:41:14 +0000

    feat(tracker): wire lesson detail JSON URL

--- tracker/urls.py
+++ tracker/urls.py
@@ -7,4 +7,5 @@ app_name = 'tracker'
 urlpatterns = [
     path('calendar/', views.calendar_view, name='calendar'),
     path('calendar/<int:year>/<int:week>/', views.calendar_view, name='calendar_week'),
+    path('lessons/<int:scheduled_id>/detail/', views.lesson_detail_view, name='lesson_detail'),
 ]
