d286b5b9b2c748481643f4b72a51db1eb975e0d7
adelcareers <adel.careers@gmail.com>
2026-03-09 15:42:17 +0000

    feat(tracker): wire student notes URL

--- tracker/urls.py
+++ tracker/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('lessons/<int:scheduled_id>/detail/', views.lesson_detail_view, name='lesson_detail'),
     path('lessons/<int:scheduled_id>/update/', views.update_lesson_status_view, name='lesson_update'),
     path('lessons/<int:scheduled_id>/mastery/', views.update_mastery_view, name='lesson_mastery'),
+    path('lessons/<int:scheduled_id>/notes/', views.save_notes_view, name='lesson_notes'),
 ]
