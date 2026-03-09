7fe3e834fa655d7f3647eaa0dc49b50dbb7cc08b
adelcareers <adel.careers@gmail.com>
2026-03-09 15:32:59 +0000

    feat(tracker): wire mastery update URL

--- tracker/urls.py
+++ tracker/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('calendar/<int:year>/<int:week>/', views.calendar_view, name='calendar_week'),
     path('lessons/<int:scheduled_id>/detail/', views.lesson_detail_view, name='lesson_detail'),
     path('lessons/<int:scheduled_id>/update/', views.update_lesson_status_view, name='lesson_update'),
+    path('lessons/<int:scheduled_id>/mastery/', views.update_mastery_view, name='lesson_mastery'),
 ]
